8c2a67e2d71afce583ce7d5df2c568b9809921b6
Ankur Kathiriya <kathiriya.ankur@gmail.com>
2021-11-09 14:16:29 +0530

    add framework

---
+++ Jackspa22ow/Jackspa22ow/Jackspa22ow.h
@@ -0,0 +1,18 @@
+//
+//  Jackspa22ow.h
+//  Jackspa22ow
+//
+//  Created by Ankur Kathiriya on 09/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Jackspa22ow.
+FOUNDATION_EXPORT double Jackspa22owVersionNumber;
+
+//! Project version string for Jackspa22ow.
+FOUNDATION_EXPORT const unsigned char Jackspa22owVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jackspa22ow/PublicHeader.h>
+
+
---
+++ Jackspa22ow/Jackspa22ow/Jackspa22ow.swift
@@ -0,0 +1,14 @@
+//
+//  Jackspa22ow.swift
+//  Jackspa22ow
+//
+//  Created by Ankur Kathiriya on 09/11/21.
+//
+
+import Foundation
+
+public struct Jackspa22ow {
+    public static func sayHello(){
+        print("Hello pretty lady. How are you doing?")
+    }
+}
